f46fc6133d7263cefa8955a3e697b45c841d1b44
Rémi Castaing <remi.castaing@gmail.com>
2016-12-30 22:27:38 +0100

    add navbar, signin, signup

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,17 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes = [
+  {path: '', component: HomeComponent}, // load the home route by default
+  {path: 'home', component: HomeComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'signin', component: SigninComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes),
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
