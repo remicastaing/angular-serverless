a3b0b64e857c406b323348c700daefd6a384fe0b
Rémi Castaing <remi.castaing@gmail.com>
2017-01-23 23:32:58 +0100

    add things

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'home',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	public awesomeThings: Observable<any>= new Observable();
+
+
+  constructor(private http: Http) { }
 
   ngOnInit() {
+  	this.awesomeThings = this.http.get('/api/things')
+  	.map(res => res.json());
   }
 
 }
